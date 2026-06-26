c2276c25181f05573a5526e914b357897e86f805
Nate Mortensen <natemort@uber.com>
2026-06-26 09:08:40 -0700

    test: Speed up TestSignalWithStartWorkflow_IDReusePolicy (#8293)
    <!-- If you are new to contributing or want a refresher, please read
    ./pull_request_guidance.md -->
    **What changed?**
    Avoid relying on activity timeouts.
    
    
    **Why?**
    Similar to https://github.com/cadence-workflow/cadence/pull/8290 , but a
    slightly different snippit.
    
    **How did you test it?**
    Running the tests.
    
    **Potential risks**
    Low. It's unlikely this introduces flakiness as the test relies on the
    completion of this workflow.
    
    **Release notes**
    
    
    **Documentation Changes**

--- host/signal_workflow_test.go
+++ host/signal_workflow_test.go
@@ -1558,6 +1558,11 @@ func (s *IntegrationSuite) TestSignalWithStartWorkflow_IDReusePolicy() {
 	_, err := poller.PollAndProcessDecisionTask(false, false)
 	s.Logger.Info("PollAndProcessDecisionTask", tag.Error(err))
 	s.Nil(err)
+	// Run the activity instead of waiting for it to time out
+	err = poller.PollAndProcessActivityTask(false)
+	s.Logger.Info("PollAndProcessActivityTask", tag.Error(err))
+	s.Nil(err)
+
 	_, err = poller.PollAndProcessDecisionTask(false, false)
 	s.Logger.Info("PollAndProcessDecisionTask", tag.Error(err))
 	s.Nil(err)
